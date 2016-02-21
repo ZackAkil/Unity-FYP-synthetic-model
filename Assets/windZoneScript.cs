92cf40dd56ee36d60d79027e86efb1bdcb3d496d
ZackAkil <zackakil@Zacks-MacBook-Pro.local>
2016-02-21 10:56:15 +0000

    added wind zone script file to wind zone prefab

---
+++ Assets/windZoneScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class windZoneScript : MonoBehaviour {
+
+	// Use this for initialization
+	public int streetDirection = 0;
+	public int streetWidth = 5;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
