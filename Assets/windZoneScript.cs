c9b39ad5f903a7845cb4b19170117508a22f4839
ZackAkil <zackakil94@gmail.com>
2016-02-24 10:15:04 +0000

    Implement function for street wind direction

--- Assets/windZoneScript.cs
+++ Assets/windZoneScript.cs
@@ -29,12 +29,12 @@ public class windZoneScript : MonoBehaviour {
 
 	//using global wind condition object to calculate wind speed within street
 
-	static float calculateStreetWindSpeed (float gWindDir, float gWindSpeed, float steetDir, float streetWdth){
+	static float calculateStreetWindSpeed (float gWindDir, float gWindSpeed, float streetDir, float streetWdth){
 
-		float gausian1 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-steetDir)/streetWdth),2f));
-		float gausian2 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-steetDir+180f)/streetWdth),2f)); 
-		float gausian3 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-steetDir-180f)/streetWdth),2f)); 
-		float gausian4 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-steetDir-360f)/streetWdth),2f)); 
+		float gausian1 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-streetDir)/streetWdth),2f));
+		float gausian2 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-streetDir+180f)/streetWdth),2f)); 
+		float gausian3 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-streetDir-180f)/streetWdth),2f)); 
+		float gausian4 = Mathf.Pow(2f,Mathf.Pow(-((gWindDir-streetDir-360f)/streetWdth),2f)); 
 
 		float[] gausian = {gausian1,gausian2,gausian3,gausian4};
 
@@ -44,9 +44,12 @@ public class windZoneScript : MonoBehaviour {
 
 	//using global wind condition object to calculate wind direction within street
 
-	float calculateStreetWindDirection (float gWindDir, float gWindSpeed, float steetDir, float streetWdth){
+	static float calculateStreetWindDirection (float gWindDir, float gWindSpeed, float streetDir, float streetWdth){
 
-	
-		return 0f;
+		float sig1 = (1/(1+ Mathf.Pow(Mathf.Exp(1),(-(gWindDir-streetDir+90)*streetWdth))))*180; 
+		float sig2 = (1/(1+ Mathf.Pow(Mathf.Exp(1),(-(gWindDir-streetDir-90)*streetWdth))))*180; 
+		float sig3 = (1/(1+ Mathf.Pow(Mathf.Exp(1),(-(gWindDir-streetDir-270)*streetWdth))))*180; 
+
+		return (sig1 + sig2 + sig3 - 180 + streetDir)%360;
 	}
 }
