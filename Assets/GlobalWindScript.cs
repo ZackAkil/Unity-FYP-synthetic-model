ec3d8ead9780459c480f47528886f325180d1ddf
ZackAkil <zackakil94@gmail.com>
2016-02-24 10:40:49 +0000

    update meathods

--- Assets/GlobalWindScript.cs
+++ Assets/GlobalWindScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class globalWindScript : MonoBehaviour {
+public class GlobalWindScript : MonoBehaviour {
 
 	public float windSpeed;
 	public float windDir;
