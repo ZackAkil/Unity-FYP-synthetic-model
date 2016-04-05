8853989584669ed29d4bde015e4fc7821c737de5
ZackAkil <zackakil94@gmail.com>
2016-04-05 16:36:20 +0100

    added base predicted zone object

---
+++ Assets/zonePredictionScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zonePredictionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
